b24b8eb3075f61c1076dd3561e82e76bc5e87ab8
adexterity <99201855+adexterity@users.noreply.github.com>
2022-02-25 06:42:35 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-var btn= document.querySelector('.btn');
-var numb=document.querySelector('#numb');
-
-btn.addEventListener('click', function(){
-    
-    var numbers= Math.floor(Math.random() * 1000);
-    numb.value= numbers;
-})
